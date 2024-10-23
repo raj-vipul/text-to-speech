ce6f092463a975e65897e80e57f5f7dd0088f902
Vipul raj <161920868+raj-vipul@users.noreply.github.com>
2024-10-23 14:04:15 +0530

    Added files

---
+++ text to speech/script.js
@@ -0,0 +1,19 @@
+
+let speech = new SpeechSynthesisUtterance();
+
+document.querySelector("button").addEventListener("click", () => {
+    let text = document.querySelector("textarea").value;
+    
+       if (text.trim() === "") {
+        alert("Please enter some text.");
+        return;
+    }
+
+      speech.text = text;
+
+       speech.lang = "en-usa";  
+     window.speechSynthesis.cancel();
+    
+     window.speechSynthesis.speak(speech);
+});
+
